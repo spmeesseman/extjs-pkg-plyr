becd0a2a6b19548526ad6d914866c443e50bb7a6
spmeesseman <spmeesseman@gmail.com>
2019-04-18 17:47:13 -0400

    progress: build out inner plyr api to extjs component level (untested/non-working) [skip ci]

--- src/Plyr.js
+++ src/Plyr.js
@@ -50,10 +50,12 @@ Ext.define('Ext.plyr.Plyr',
 
     getIdRoot: function()
     {
-		if (this.idRoot === 0) {
-			this.idRoot = ++Ext.plyr.Plyr.playerIdCounter;
+		var me = this;
+		if (!me.idRoot) {
+			me.idRoot = ++Ext.plyr.Plyr.playerIdCounter;
+			me.playerId = 'player_' + me.idRoot.toString();
 		}
-        return this.idRoot;
+        return me.idRoot;
 	},
 	
 	bind:
@@ -75,7 +77,248 @@ Ext.define('Ext.plyr.Plyr',
         '</object>'
 	},
 	
-    afterRender: function() 
+	listeners:
+	{
+		destroy: function(cmp, eopts)
+		{
+			console.error('CHECK TO MAKE SURE THIS IS BEING CALLED!!!  REMOVE WHEN CONFIRMED');
+			if (me.player) {
+				console.warning('REMOVE WHEN CONFIRMED');
+				me.player.destroy();
+			}
+		}
+	},
+
+	//
+	// List of available plyr API methods:
+	//
+	//     play()¹	           -	Start playback.
+    //     pause()	           -	Pause playback.
+    //     stop()	           -	Stop playback and reset to start.
+    //     restart()	       -	Restart playback.
+    //     fullscreen.enter()	-	Enter fullscreen. If fullscreen is not supported, a fallback "full window/viewport" is used instead.
+    //     fullscreen.exit()	-	Exit fullscreen.
+    //     fullscreen.toggle()	-	Toggle fullscreen.
+    //     airplay()	        -	Trigger the airplay dialog on supported devices.
+    //     destroy()	        -	Destroy the instance and garbage collect any elements.
+    //     togglePlay(toggle)	    Boolean	Toggle playback, if no parameters are passed, it will toggle based on current status.
+    //     rewind(seekTime)	        Number  Rewind playback by the specified seek time. If no parameter is passed, the default seek time will be used.
+    //     forward(seekTime)	    Number	Fast forward by the specified seek time. If no parameter is passed, the default seek time will be used.
+    //     increaseVolume(step)	    Number	Increase volume by the specified step. If no parameter is passed, the default step will be used.
+    //     decreaseVolume(step)	    Number	Increase volume by the specified step. If no parameter is passed, the default step will be used.
+    //     toggleCaptions(toggle)   Boolean	Toggle captions display. If no parameter is passed, it will toggle based on current status.
+    //     toggleControls(toggle)   Boolean	Toggle the controls (video only). Takes optional truthy value to force it on/off.
+    //     on(event, function)	    String, Function	Add an event listener for the specified event.
+    //     once(event, function)    String, Function	Add an event listener for the specified event once.
+    //     off(event, function)	    String, Function	Remove an event listener for the specified event.
+    //     supports(type)	        String	Check support for a mime type.
+	//
+
+
+	taskRunner: null,
+	taskRunnerTask: null,
+	taskRunnerTaskStep: 2,
+
+	ffwd: function(seconds)
+	{
+		var me = this;
+		
+		if (me.plyrLog) {
+			me.plyrLog("PLYR: Command received - Fast Forward", 1);
+		}
+		if (!me.player) {
+			if (me.plyrLog) {
+				me.plyrLog("PLYR: No active player", 1);
+			}
+			return;
+		}
+
+		if (!seconds) {
+			seconds = me.taskRunnerTaskStep;
+		}
+		if (!seconds) {
+			seconds = 2;
+		}
+
+		me.player.forward(seconds);
+	},
+
+
+	ffwdStart: function(seconds, intervalms)
+	{
+		var me = this;
+
+		if (me.plyrLog) {
+			me.plyrLog("PLYR: Command received - Fast Forward Start", 1);
+		}
+		if (!me.player) {
+			if (me.plyrLog) {
+				me.plyrLog("PLYR: No active player", 1);
+			}
+			return;
+		}
+
+		me.pause();
+		me.ffwd(seconds);
+
+		//
+		// TODO - this should actually use a webworker in the case the browser window
+		// loses focus or is not the active window
+		//
+		if (!me.taskRunner) {
+			me.taskRunner = new Ext.util.TaskRunner();	
+		}
+		if (!me.taskRunnerTask) {
+			me.taskRunnerTaskStep = seconds;
+			me.taskRunnerTask = me.taskRunner.start({
+				run: me.ffwd,
+				interval: intervalms ? intervalms : 500
+			});
+		}
+		else {
+			// TODO
+		}
+	},
+
+
+	ffwdRwdStop: function()
+	{
+		var me = this;
+
+		if (me.plyrLog) {
+			me.plyrLog("PLYR: Command received - Fast Forward / Rewind Stop", 1);
+		}
+		if (!me.player) {
+			if (me.plyrLog) {
+				me.plyrLog("PLYR: No active player", 1);
+			}
+			return;
+		}
+
+		if (me.taskRunner && me.taskRunnerTask)
+		{
+			me.taskRunner.stop(me.taskRunnerTask, true);
+			me.taskRunnerTask = null;
+			me.taskRunnerTaskStep = 1;
+			me.play(); // TODO - should pick up in state where we were on Start()
+		}
+	},
+
+
+	pause: function()
+	{
+		var me = this;
+		
+		if (me.plyrLog) {
+			me.plyrLog("PLYR: Command received - Resume", 1);
+		}
+		if (!me.player) {
+			if (me.plyrLog) {
+				me.plyrLog("PLYR: No active player", 1);
+			}
+			return;
+		}
+
+		me.player.pause();
+	},
+
+
+	play: function()
+	{
+		var me = this;
+		
+		if (me.plyrLog) {
+			me.plyrLog("PLYR: Command received - Resume", 1);
+		}
+		if (!me.player) {
+			if (me.plyrLog) {
+				me.plyrLog("PLYR: No active player", 1);
+			}
+			return;
+		}
+
+		me.player.play();
+	},
+
+
+	togglePlay: function()
+	{
+		var me = this;
+		
+		if (me.plyrLog) {
+			me.plyrLog("PLYR: Command received - Play/Pause", 1);
+		}
+		if (!me.player) {
+			if (me.plyrLog) {
+				me.plyrLog("PLYR: No active player", 1);
+			}
+			return;
+		}
+
+		me.player.togglePlay();
+	},
+
+
+	rwd: function(seconds)
+	{
+		var me = this;
+		
+		if (me.plyrLog) {
+			me.plyrLog("PLYR: Command received - Rewind", 1);
+		}
+		if (!me.player) {
+			if (me.plyrLog) {
+				me.plyrLog("PLYR: No active player", 1);
+			}
+			return;
+		}
+
+		if (!seconds) {
+			seconds = me.taskRunnerTaskStep;
+		}
+
+		me.player.rewind(seconds);
+	},
+
+
+	rwdStart: function(seconds, intervalms)
+	{
+		var me = this;
+
+		if (me.plyrLog) {
+			me.plyrLog("PLYR: Command received - Fast Forward", 1);
+		}
+		if (!me.player) {
+			if (me.plyrLog) {
+				me.plyrLog("PLYR: No active player", 1);
+			}
+			return;
+		}
+
+		me.pause();
+		me.rwd(seconds);
+
+		//
+		// TODO - this should actually use a webworker in the case the browser window
+		// loses focus or is not the active window
+		//
+		if (!me.taskRunner) {
+			me.taskRunner = new Ext.util.TaskRunner();	
+		}
+		if (!me.taskRunnerTask) {
+			me.taskRunnerTaskStep = seconds;
+			me.taskRunnerTask = me.taskRunner.start({
+				run: me.rwd,
+				interval: intervalms ? intervalms : 500
+			});
+		}
+		else {
+			// TODO
+		}
+	},
+
+
+	afterRender: function() 
     {
     	var me = this;
     	me.callParent(arguments);
@@ -115,7 +358,10 @@ Ext.define('Ext.plyr.Plyr',
     	}
     	else
     	{
-    		me.onLoadSuccess();
+    		new Ext.util.DelayedTask(function()
+    		{
+				me.onLoadSuccess();
+			}, me).delay(25);
     	}
     },
 
@@ -167,7 +413,6 @@ Ext.define('Ext.plyr.Plyr',
 			//
 			// JS and CSS both loaded, initialize the plyr component
 			//
-			me.playerId = '#player_' + me.getIdRoot();
 			if (me.plyrLog) {
 				me.plyrLog("   CSS loaded", 1);
 			}
@@ -176,9 +421,42 @@ Ext.define('Ext.plyr.Plyr',
     	}
 		else if (Ext.plyr.Plyr.sriptLoaded === true)
 		{
-			me.player = new Plyr(me.playerId);
+			var opts = {
+				enabled: true,           // Whether to completely disable Plyr
+				debug: false,            // Display debugging information in the console
+				autoplay: false,         // Autoplay the media on load. This is generally advised against
+				autopause: true,         // Only allow one player playing at once
+				muted: false,            // Whether to start playback muted
+				hideControls: true,      // Hide video controls automatically after 2s of no mouse
+				clickToPlay: true,       // Click (or tap) of the video container will toggle play/pause
+				disableContextMenu: true,// Disable right click menu on video
+				resetOnEnd: false,       // Reset the playback to the start once playback is complete
+				seekTime: 10,            // The time, in seconds, to seek when a user hits fast forward or rewind
+				volume: 1,               // A number, between 0 and 1, representing the initial volume of the player
+				duration: null,          // Specify a custom duration for media.
+				displayDuration: true,   // Displays the duration of the media on the "metadataloaded" event (on startup) in the current time display. This will only work if the preload attribute is not set to none (or is not set at all) and you choose not to display the duration (see controls option).
+				invertTime: true,        // Display the current time as a countdown rather than an incremental counter
+				//
+				// If a function is passed, it is assumed your method will return either an element or HTML string 
+				// for the controls. Three arguments will be passed to your function; id (the unique id for the 
+				// player), seektime (the seektime step in seconds), and title (the media title). See controls.md 
+				//for more info on how the html needs to be structured.
+				//
+				controls: ['play-large', 'play', 'progress', 'current-time', 'mute', 'volume', 'captions', 'settings', 'pip', 'airplay', 'fullscreen'],
+				// If using the default controls are used then you can specify which settings to show in the menu
+				settings: ['captions', 'quality', 'speed', 'loop'],
+				keyboard: { focused: true, global: false }, // Enable keyboard shortcuts for focused players only or globally
+				tooltips: { controls: false, seek: true },  // Display control labels as tooltips on :hover & :focus (by default, the labels are screen reader only). seek: Display a seek tooltip to indicate on click where the media would seek to.
+				storage:  { enabled: true, key: 'plyr' },   // enabled: Allow use of local storage to store user settings. key: The key name to use.
+				captions: { active: false, language: 'auto', update: false }
+			};
+
+			//me.player = new Plyr('#' + me.playerId, opts);
+			me.player = new Plyr('#' + me.playerId);
+
 			if (me.plyrLog) {
 				me.plyrLog("    Player initialized", 1);
+				me.plyrLog("       ID: " + me.playerId, 1);
 			}
 			if (me.plyrOnLoaded) {
 				if (me.plyrOnLoaded instanceof Function) {
