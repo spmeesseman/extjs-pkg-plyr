a6b2b1f022a6f0ec4b5b2ffd157ad46b26529775
spmeesseman <spmeesseman@gmail.com>
2019-12-21 17:30:39 -0500

    build: Inlcude minified and polyfilled javascript files in speed array change

--- build.sh
+++ build.sh
@@ -11,6 +11,12 @@ fi
 echo "Copying Plyr dist to local package dir"
 cp -f $PLYRDIR/dist/* plyr
 
+#
+# Apply more speed settings
+#
 sed -i 's/0.5, 0.75, 1, 1.25, 1.5, 1.75, 2/0.5, 0.6, 0.7, 0.8, 0.9, 1, 1.1, 1.25, 1.5, 1.75, 2/g' plyr/plyr.js
+sed -i 's/.5,.75,1,1.25,1.5,1.75,2/.5,.6,.7,.8,.9,1,1.1,1.25,1.5,1.75,2/g' plyr/plyr.min.js
+sed -i 's/0.5, 0.75, 1, 1.25, 1.5, 1.75, 2/0.5, 0.6, 0.7, 0.8, 0.9, 1, 1.1, 1.25, 1.5, 1.75, 2/g' plyr/plyr.polyfilled.js
+sed -i 's/.5,.75,1,1.25,1.5,1.75,2/.5,.6,.7,.8,.9,1,1.1,1.25,1.5,1.75,2/g' plyr/plyr.polyfilled.min.js
 
 exit
